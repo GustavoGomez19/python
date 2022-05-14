ee82559f35ad96e8f0656f30f8b6a8f911379d3d
GustavoGomez19 <gagomezq1992@gmail.com>
2022-05-13 19:25:20 -0500

    Desarrollo reto 2

--- Reto2/reto2.py
+++ Reto2/reto2.py
@@ -14,28 +14,69 @@
 #print(dicc1)
 
 def cliente(d):
-
+  
   diccionario={}
   diccionario["nombre"]=d["nombre"]
   diccionario["edad"]=d["edad"]
+
+#Atracciones
   if d["edad"]>18:
     diccionario["atraccion"]="x-treme"
+    diccionario["apto"]=True
+    diccionario["primer_ingreso"]=d["primer_ingreso"]
+
   if d["edad"]>=15 and d["edad"]<=18:
-    diccionario["atraccion"]="Carros chocones"
-  if d["edad"]>=7 and d["edad"]<15:
+    diccionario["atraccion"]="Carroschocones"
+    diccionario["apto"]=True
+    diccionario["primer_ingreso"]=d["primer_ingreso"]
+
+  if d["edad"]>=7 and d["edad"]<=15:
     diccionario["atraccion"]="Sillas voladoras"
+    diccionario["apto"]=True
+    diccionario["primer_ingreso"]=d["primer_ingreso"]
+
+#Precio entradas
+  if d["edad"]>18 and d["primer_ingreso"]==True:
+    diccionario["total_boleta"]=20000*.95
+
+  if d["edad"]>18 and d["primer_ingreso"]==False:
+    diccionario["total_boleta"]=20000
+
+  if d["edad"]>=15 and d["edad"]<=18 and d["primer_ingreso"]==True:
+    diccionario["total_boleta"]=5000*.93
+
+  if d["edad"]>=15 and d["edad"]<=18 and d["primer_ingreso"]==False:
+    diccionario["total_boleta"]=5000
+
+  if d["edad"]>=7 and d["edad"]<=15 and d["primer_ingreso"]==True:
+    diccionario["total_boleta"]=10000*.95
+
+  if d["edad"]>=7 and d["edad"]<=15 and d["primer_ingreso"]==False:
+    diccionario["total_boleta"]=10000
+
   if d["edad"]<7:
     diccionario["atraccion"]="N/A"
-  if d["apto"]:
-    diccionario["apto"]=True
-  diccionario["apto"]=d["apto"]
-  if d["primer_ingreso"]:
+    diccionario["apto"]=False
     diccionario["primer_ingreso"]=True
-  diccionario["primer_ingreso"]=d["primer_ingreso"]
+    diccionario["total_boleta"]="N/A"
+
 
 
   return diccionario
 
 #Codigo ppal
-dic1={"id_cliente":1,"nombre":"Johana Fernandez","edad":20,"primer_ingreso":True,"apto":True,}
+dic1={"id_cliente":1,"nombre":"Johana Fernandez","edad":20,"primer_ingreso":True}
+dic2={"id_cliente":1,"nombre":"Johana Fernandez","edad":20,"primer_ingreso":False}
+dic3={"id_cliente":2,"nombre":"Gloria Suarez","edad":3,"primer_ingreso":True}
+dic4={"id_cliente":3,"nombre":"Tatiana Suarez","edad":17,"primer_ingreso":True}
+dic5={"id_cliente":3,"nombre":"Tatiana Suarez","edad":17,"primer_ingreso":False}
+dic6={"id_cliente":4,"nombre":"Tatiana Ruiz","edad":8,"primer_ingreso":True}
+dic7={"id_cliente":4,"nombre":"Tatiana Ruiz","edad":8,"primer_ingreso":False}
+
 print(cliente(dic1))
+print(cliente(dic2))
+print(cliente(dic3))
+print(cliente(dic4))
+print(cliente(dic5))
+print(cliente(dic6))
+print(cliente(dic7))
